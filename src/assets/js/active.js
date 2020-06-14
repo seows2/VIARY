ac7f581c9462c4e22c0b7a1a42307d2da65915ee
SeoWS <seows2@naver.com>
2020-06-14 22:24:33 +0900

    group animation & active

--- src/assets/js/active.js
+++ src/assets/js/active.js
@@ -1,13 +1,15 @@
 const privateButton = document.querySelector(".private");
 
-const privateClick = (event) => {
-  console.log(event);
+const privateClick = () => {
+  const active = document.querySelector(".active");
+  if (active) {
+    active.classList.remove(".active");
+    privateButton.classList.add("active");
+  } else {
+    privateButton.classList.add("active");
+  }
 };
 
-const activeInit = () => {
-  privateButton.addEventListener("click", privateClick);
-};
-
-if (privateButton) {
-  activeInit();
+if (location.pathname.indexOf("group") !== -1) {
+  privateClick();
 }
